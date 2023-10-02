9112be59f9edba8cd2f96cace02e99d84675586e
David Gregs <108700012+davidgregs87@users.noreply.github.com>
2023-10-02 21:29:47 +0100

    all done and completed

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) => res.map((prom) => ({
+    status: prom.status,
+    value: prom.status === 'fulfilled' ? prom.value : String(prom.reason),
+  })));
+}
